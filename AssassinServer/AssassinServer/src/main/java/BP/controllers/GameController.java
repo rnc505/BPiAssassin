842d0334d39ef370ec60a76dc69f5c950b3e6ad0
Robby Cohen <robert.n.cohen@vanderbilt.edu>
2014-03-12 15:56:13 -0500

    Game Controller updated

--- AssassinServer/AssassinServer/src/main/java/BP/controllers/GameController.java
+++ AssassinServer/AssassinServer/src/main/java/BP/controllers/GameController.java
@@ -3,35 +3,31 @@ package BP.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import BP.users.*;
-import BP.repository.*;
-import BP.game.*;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import BP.domain.GameData;
+import BP.events.GameManager;
+import BP.events.GameManagerInterface;
+import BP.users.GameUserImage;
 
 
 @Controller
 @RequestMapping("/game")
 public class GameController {
 	
+	GameManagerInterface gameManager;
 	public GameController() {
+		this.gameManager = new GameManager();
 	}
 	
 	
 	/**
 	 * createGame - Creates a game instance and passes user image data back to host for initializing the 
 	 * facial recognizer.
-	 * @param json - String containing the JSON of the request
 	 * 
 	 * Expected format of JSON:
 	 *	 {
@@ -41,18 +37,54 @@ public class GameController {
 	 *					"7ed41362-639c-461e-85f8-53ca2613c9f5"],
 	 *			"hostId": "4a35e8b0-a958-11e3-a5e2-0800200c9a66"
 	 *	 }
+	 *
+	 *	@return GameCreated Object
 	 */
 	
 	@RequestMapping(method = RequestMethod.POST, value = "createGame")
-	public @ResponseBody List<GameUserImage> createGame(
+	public @ResponseBody GameCreated createGame(
 			@RequestParam(value = "hostId",required = true, defaultValue = "") final String hostID,
 			@RequestParam(value = "playerIds", required = true, defaultValue = "") final ArrayList<String> playerIds
 			){
 	
-		return null;
+		return gameManager.createGame(hostID, playerIds);
 		
 	}
 	
-	
-	
+	/**
+	 * startGame - Starts a game with the initialized values from Facial Recognizer
+	 * 
+	 * The following four parameters are the base64-encoded binary data required by the recognizer to
+	 * make the game work:
+	 * 
+	 * - meanImage
+	 * - covarianceEigenvectors
+	 * - largestEigenvectorsOfTheWorkFunction
+	 * - projectedImages
+	 * 
+	 * Expected format of JSON:
+	 *	 {
+	 *			"gameId": "4a35e8b0-a958-11e3-a5e2-0800200c9a66",
+	 *			"meanImage": "ImdhbWVJZCI6ICI0YTM1ZThiMC1hOTU4LTExZTMtYTVlMi0wODAwMjAwYzlhNjYiImdhbWVJZCI6ICI0YTM1ZThiMC1hOTU4LTExZTMtYTVlMi0wODAwMjAwYzlhNjYi",
+	 *			"covarEigen": "ImdhbWVJZCI6ICI0YTM1ZThiMC1hOTU4LTExZTMtYTVlMi0wODAwMjAwYzlhNjYiImdhbWVJZCI6ICI0YTM1ZThiMC1hOTU4LTExZTMtYTVlMi0wODAwMjAwYzlhNjYi",
+	 *			"workFunctEigen": "ImdhbWVJZCI6ICI0YTM1ZThiMC1hOTU4LTExZTMtYTVlMi0wODAwMjAwYzlhNjYiImdhbWVJZCI6ICI0YTM1ZThiMC1hOTU4LTExZTMtYTVlMi0wODAwMjAwYzlhNjYi",
+	 *			"projectedImages": "ImdhbWVJZCI6ICI0YTM1ZThiMC1hOTU4LTExZTMtYTVlMi0wODAwMjAwYzlhNjYiImdhbWVJZCI6ICI0YTM1ZThiMC1hOTU4LTExZTMtYTVlMi0wODAwMjAwYzlhNjYi"
+	 *	 }
+	 *
+	 *	@return 
+	 */
+	@RequestMapping(method = RequestMethod.POST, value = "createGame")
+	public @ResponseBody String startGame(
+			@RequestParam(value = "gameId",required = true, defaultValue = "") final String gameId,
+			@RequestParam(value = "meanImage", required = true, defaultValue = "") final String meanImage,
+			@RequestParam(value = "covarEigen", required = true, defaultValue = "") final String covarEigen,
+			@RequestParam(value = "workFunctEigen", required = true, defaultValue = "") final String workFunctEigen,
+			@RequestParam(value = "projectedImages", required = true, defaultValue = "") final String projectedImages
+			){
+		
+		GameData recognizerData = new GameData(meanImage, covarEigen, workFunctEigen, projectedImages);
+		gameManager.startGame(gameId, recognizerData);
+		return null;
+		
+	}
 }
