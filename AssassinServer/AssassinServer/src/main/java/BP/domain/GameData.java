b64c044c48f227f60c7328c698bacca454b56e72
Robby Cohen <robert.n.cohen@vanderbilt.edu>
2014-03-17 15:25:13 -0500

    Added GameData Ctor

--- AssassinServer/AssassinServer/src/main/java/BP/domain/GameData.java
+++ AssassinServer/AssassinServer/src/main/java/BP/domain/GameData.java
@@ -1,5 +1,18 @@
 package BP.domain;
 
 public class GameData {
+
+	String meanImage;
+	String covarEigen;
+	String workFunctEigen;
+	String projectedImages;
+	
+	public GameData(String meanImage, String covarEigen, String workFunctEigen,
+			String projectedImages) {
+		this.meanImage = meanImage;
+		this.covarEigen = covarEigen;
+		this.workFunctEigen = workFunctEigen;
+		this.projectedImages = projectedImages;
+	}
 	
 }
