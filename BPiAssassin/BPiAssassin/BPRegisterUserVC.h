0b90bfa4bb5362a89505aa12e8f3299b1b4c3ae0
John Rozier <John@Johns-MacBook-Pro-7.local>
2014-04-03 20:53:34 -0500

    Added Register User VC

---
+++ BPiAssassin/BPiAssassin/BPRegisterUserVC.h
@@ -0,0 +1,20 @@
+//
+//  BPRegisterUserVC.h
+//  BPiAssassin
+//
+//  Created by John Rozier on 4/3/14.
+//  Copyright (c) 2014 BP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BPRegisterUserVC : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *usr_code_name;
+
+@property (strong, nonatomic) IBOutlet UIImageView *usrImage1;
+@property (strong, nonatomic) IBOutlet UIImageView *usrImage2;
+@property (strong, nonatomic) IBOutlet UIImageView *usrImage3;
+@property (strong, nonatomic) IBOutlet UIImageView *usrImage4;
+
+@end
